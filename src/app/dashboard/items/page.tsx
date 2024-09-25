f723ab6411b71af9b1544026c9ccdd6b2bac4ac5
kasurbantal <kasurbantal880@gmail.com>
2024-09-25 22:21:04 +0700

    feat: add SearchBar

--- src/app/dashboard/items/page.tsx
+++ src/app/dashboard/items/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ItemTable from "@/components/Items";
 import Header from "@/components/Header";
-import ItemSearchBar from "@/components/ItemSearchBar";
+import SearchBar from "@/components/SearchBar";
 
 const Page = () => {
   return (
     <div>
       <h1>Inventory Items</h1>
       <Header title="Items" button_name="Add Item" />
-      <ItemSearchBar />
+      <SearchBar placeholder="Search item ID.." isStockPage={false} />
       <ItemTable />
     </div>
   );
--- src/app/dashboard/stocks/page.tsx
+++ src/app/dashboard/stocks/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ItemTable from "@/components/Items";
 import Header from "@/components/Header";
-import ItemSearchBar from "@/components/ItemSearchBar";
+import SearchBar from "@/components/SearchBar";
 
 const Page = () => {
   return (
     <div>
       <h1>Inventory Stocks</h1>
       <Header title="Stocks" button_name="New Stock" />
-      <ItemSearchBar />
+      <SearchBar placeholder="Quick search..." isStockPage={true} />
       <ItemTable />
     </div>
   );
