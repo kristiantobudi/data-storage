9d0c68661da96c5d7c9e326e030896604bc82687
kasurbantal <kasurbantal880@gmail.com>
2024-09-25 21:38:33 +0700

    fix: add title props

--- src/components/ItemHeader.tsx
+++ src/components/ItemHeader.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const ItemHeader: React.FC = () => {
+interface HeaderProps {
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <div className="p-4 bg-white text-purple-950 flex flex-row justify-between">
-      <h2 className="text-2xl font-bold">Items</h2>
+      <h2 className="text-2xl font-bold">{title}</h2>
       <button className="bg-purple-800 text-white rounded p-2 transition-all hover:bg-purple-400 hover:text-indigo-950">
         + | Add Item
       </button>
@@ -11,4 +15,4 @@ const ItemHeader: React.FC = () => {
   );
 };
 
-export default ItemHeader;
+export default Header;
