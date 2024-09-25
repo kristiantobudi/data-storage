403e5c7fee323b7b6b68d970bc777b9736d2c113
kasurbantal <kasurbantal>
2024-09-25 13:16:56 +0700

    feat:add date icon

--- src/components/ItemSearchBar.tsx
+++ src/components/ItemSearchBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdOutlineDateRange } from "react-icons/md";
 
 const ItemSearchBar: React.FC = () => {
   return (
@@ -7,16 +8,25 @@ const ItemSearchBar: React.FC = () => {
         placeholder="Search item id..."
         className="w-1/3 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
       />
-      <div className="flex flex-row space-x-2">
-        <button className="px-8 bg-white text-purple-800 border-purple-800 rounded p-2 transition-all hover:bg-purple-400 hover:text-white">
-          Status
-        </button>
-        <button className="px-8 bg-white text-purple-800 border-purple-800 rounded p-2 transition-all hover:bg-purple-400 hover:text-white">
-          Sales
-        </button>
-        <button className="px-8 bg-white text-purple-800 border-purple-800 rounded p-2 transition-all hover:bg-purple-400 hover:text-white">
-          Filter
-        </button>
+      <div className="flex flex-row space-x-2 gap-2">
+        <div className="px-2 py-2 border-purple-800 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500 transition-al justify-center items-center">
+          <MdOutlineDateRange className="w-8 h-8"/>
+        </div>
+        <select className="px-4 py-2 bg-white text-purple-800 border-purple-800 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500 transition-all">
+          <option value="">Sales</option>
+          <option value="">Sales1</option>
+          <option value="">Sales2</option>
+        </select>
+        <select className="px-4 py-2 bg-white text-purple-800 border-purple-800 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500 transition-all">
+          <option value="">Status</option>
+          <option value="">Status1</option>
+          <option value="">Status2</option>
+        </select>
+        <select className="px-4 py-2 bg-white text-purple-800 border-purple-800 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500 transition-all">
+          <option value="">Filter</option>
+          <option value="">Filter1</option>
+          <option value="">Filter2</option>
+        </select>
       </div>
     </div>
   );
