2bc0fe48157db8d23557e7e14abb977744e21130
kasurbantal <kasurbantal>
2024-09-25 13:26:12 +0700

    feat:add search icon

--- src/components/ItemSearchBar.tsx
+++ src/components/ItemSearchBar.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { MdOutlineDateRange } from "react-icons/md";
+import { IoMdSearch } from "react-icons/io";
 
 const ItemSearchBar: React.FC = () => {
   return (
     <div className="p-4 bg-white text-purple-950 flex flex-row justify-between">
-      <input
-        placeholder="Search item id..."
-        className="w-1/3 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-      />
+       <div className="relative w-1/3 py-2">
+        <IoMdSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 w-8 h-8 text-purple-800" />
+        <input
+          placeholder="Search item id..."
+          className="w-full pl-10 pr-2 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+        />
+      </div>
       <div className="flex flex-row space-x-2 gap-2">
         <div className="px-2 py-2 border-purple-800 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500 transition-al justify-center items-center">
           <MdOutlineDateRange className="w-8 h-8"/>
