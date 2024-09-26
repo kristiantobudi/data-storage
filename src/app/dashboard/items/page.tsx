ce1468af7af33480aa38b4faa797ba6b2538c677
kasurbantal <kasurbantal880@gmail.com>
2024-09-26 23:07:03 +0700

    feat: add modal input for items

--- src/app/dashboard/items/page.tsx
+++ src/app/dashboard/items/page.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ItemTable from "@/components/Items";
 import Header from "@/components/Header";
 import SearchBar from "@/components/SearchBar";
+import AddItem from "@/components/AddItem";
 
 const Page = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddItemClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <h1>Inventory Items</h1>
-      <Header title="Items" button_name="Add Item" />
+      <Header
+        title="Items"
+        button_name="Add Item"
+        onAddItemClick={handleAddItemClick}
+      />
       <SearchBar placeholder="Search item ID.." isStockPage={false} />
       <ItemTable />
+      {isModalOpen && <AddItem closeModal={closeModal} />}
     </div>
   );
 };
