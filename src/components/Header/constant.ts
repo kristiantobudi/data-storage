48e105e3af34b1ffabcae3674393ff49903fbaac
kasurbantal <kasurbantal880@gmail.com>
2024-09-30 22:00:26 +0700

    feat: add constant.ts for keeping data

---
+++ src/components/Header/constant.ts
@@ -0,0 +1,28 @@
+import { Item } from "@/components/Header/HeaderType";
+
+export const DataItem: Item[] = [
+  {
+    item_id: 1,
+    item_name: "Product A",
+    description: "Description of Product A",
+    sku: "SKU001",
+    qty: 100,
+    category_id: 101,
+  },
+  {
+    item_id: 2,
+    item_name: "Product B",
+    description: "Description of Product B",
+    sku: "SKU002",
+    qty: 150,
+    category_id: 102,
+  },
+  {
+    item_id: 3,
+    item_name: "Product C",
+    description: "Description of Product C",
+    sku: "SKU003",
+    qty: 200,
+    category_id: 103,
+  },
+];
--- src/components/Header/useHeader.tsx
+++ src/components/Header/useHeader.tsx
@@ -1,35 +1,11 @@
 "use client";
 import { useState } from "react";
 import { Item } from "./HeaderType";
+import { DataItem } from "@/components/Header/constant";
 
 export const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [items, setItems] = useState([
-    {
-      item_id: 1,
-      item_name: "Product A",
-      description: "Description of Product A",
-      sku: "SKU001",
-      qty: 100,
-      category_id: 101,
-    },
-    {
-      item_id: 2,
-      item_name: "Product B",
-      description: "Description of Product B",
-      sku: "SKU002",
-      qty: 150,
-      category_id: 102,
-    },
-    {
-      item_id: 3,
-      item_name: "Product C",
-      description: "Description of Product C",
-      sku: "SKU003",
-      qty: 200,
-      category_id: 103,
-    },
-  ]);
+  const [items, setItems] = useState<Item[]>(DataItem);
 
   const addItem = (newItem: Item) => {
     setItems((prevItems) => [...prevItems, newItem]);
